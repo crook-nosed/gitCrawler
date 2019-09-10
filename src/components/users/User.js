77e2a0b8d97b973e9f892ccdcb3cf7c291805121
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-10 06:06:40 +0530

    added user page within ap which shows info of users.

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,22 +1,27 @@
 import React, { Component, Fragment } from "react";
 import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   };
   render() {
     // eslint-disable-next-line
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -31,7 +36,7 @@ class User extends Component {
     } = this.props.user;
 
     // eslint-disable-next-line
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
     if (loading) return <Spinner />;
     return (
       <Fragment>
@@ -65,8 +70,41 @@ class User extends Component {
             <a href={html_url} className='btn btn-dark my-1'>
               Visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
