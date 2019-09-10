820f734159fd9966dc28f938f398086a4917284d
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-10 06:04:54 +0530

    added link to user page within the app.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -39,7 +39,7 @@ const UserItem = props => {
       />
       <h3>{login}</h3>
       <div>
-        <Link to={`user/${login}`} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
         </Link>
       </div>
