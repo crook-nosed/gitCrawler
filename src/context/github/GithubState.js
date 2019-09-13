9f6824d410aeb7064de1b6b7862e7f043809185a
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-13 14:22:35 +0530

    changed github cntext so that it can be used in place of prop drilling

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -27,15 +27,37 @@ const GithubState = props => {
     );
     dispatch({
       type: SEARCH_USERS,
-      payload: res.data
+      payload: res.data.items
     });
   };
 
   // Get User
+  const getUser = async username => {
+    // this.setState({ loading: true }); in class based component
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
 
   // Get Repos
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
 
   // Clear Users
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   // Set Loading
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -46,7 +68,11 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
