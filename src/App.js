a9da1c00c90d18ffda752faf1bc20435fd5b586e
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-13 14:22:03 +0530

    moved all the state details to github context

--- src/App.js
+++ src/App.js
@@ -7,59 +7,15 @@ import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import axios from "axios";
 
 import GithubState from "./context/github/GithubState";
 import "./App.css";
 
 // refactored from class based component.
 const App = () => {
-  // state in class based component
-  // state = {
-  //   users: [],
-  //   user: {},
-  //   repos: [],
-  //   loading: false,
-  //   alert: null
-  // };
-
   // setting state in functional component
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Get single github user
-  const getUser = async username => {
-    // this.setState({ loading: true }); in class based component
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    // this.setState({ user: res.data, loading: false });
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  // Get single github user
-  const getUserRepos = async username => {
-    // this.setState({ loading: true });
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    // this.setState({ repos: res.data, loading: false }); in class based component
-    setRepos(res.data);
-    setLoading(false);
-  };
-
-  // clear users from the state
-  const clearUsers = () => {
-    // this.setState({ users: [], loading: false });
-    setUsers([]);
-    setLoading(false);
-  };
   // set alert
   const showAlert = (msg, type) => {
     // this.setState({ alert: { msg: msg, type: type } });
@@ -80,31 +36,13 @@ const App = () => {
                 path='/'
                 render={props => (
                   <Fragment>
-                    <Search
-                      searchUsers={searchUsers}
-                      clearUsers={clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={showAlert}
-                    />
-                    <Users loading={loading} users={users} />
+                    <Search setAlert={showAlert} />
+                    <Users />
                   </Fragment>
                 )}
               />
               <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={getUser}
-                    getUserRepos={getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
-              />
+              <Route exact path='/user/:login' component={User} />
             </Switch>
           </div>
         </div>
